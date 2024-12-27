a807db114ecdc0a06cb01512eccd48bfebc31c2b
Mechanical Monarchy <mechanicalmonarchydevs@gmail.com>
2024-12-27 13:52:59 -0600

    Added documentation and fixed the getTrust method (was previously using X and Y instead of X and Z).

--- src/main/java/frc/robot/subsystems/vision/CalculatedLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/CalculatedLimelight.java
@@ -20,34 +20,41 @@ public class CalculatedLimelight {
 
 
 
+  /** Initializes a CalculatedLimelight based on the given camera name. */
   public CalculatedLimelight(String key) {
     this.key = key;
   }
 
 
 
+  /** Returns the name of the camera. */
   public String getKey() {
     return key;
   }
   
+  /** Returns the Limelight NetworkTable for this camera. */
   public NetworkTable getNetworkTable() {
     return inst.getTable(key);
   }
 
+  /** Returns true if there is a target in view. */
   public boolean hasTarget() {
     return getNetworkTable().getEntry("tid").getInteger(-1) >= 0;
   }
 
+  /** Returns the ID of the currently visible target, or -1 if none are present. */
   public long getTargetID() {
     return getNetworkTable().getEntry("tid").getInteger(-1);
   }
 
+  /** Returns a value of how accurate we think the camera is at a given moment (higher value = more accurate). */
   public double getTrust() {
     double[] targetRelative = getNetworkTable().getEntry("botpose_targetspace").getDoubleArray(new double[7]);
 
-    return (1 / Math.sqrt((targetRelative[0] * targetRelative[0]) + (targetRelative[1] * targetRelative[1]) + (visionConstants.AngleDistrust * Math.sin(targetRelative[4] * (Math.PI / 180)))));
+    return (1 / Math.sqrt((targetRelative[0] * targetRelative[0]) + (targetRelative[2] * targetRelative[2]) + (visionConstants.AngleDistrust * Math.sin(targetRelative[4] * (Math.PI / 180)))));
   }
 
+  /** Returns the field-relative Pose2d. */
   public Pose2d getFieldPose() {
     double[] fieldTable = getNetworkTable().getEntry("botpose_wpiblue").getDoubleArray(new double[7]);
 
@@ -60,6 +67,7 @@ public class CalculatedLimelight {
     );
   }
 
+  /** Returns the target-relative Pose2d. */
   public Pose2d getTargetPose() {
     double[] fieldTable = getNetworkTable().getEntry("botpose_targetspace").getDoubleArray(new double[7]);
 
